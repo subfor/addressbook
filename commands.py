e6ff5f2cae7954c3c6cd41cf77382bc1df52abd5
alex <alex.kudelko@gmail.com>
2025-04-11 23:25:05 +0300

    add save notes

--- commands.py
+++ commands.py
@@ -1,12 +1,11 @@
-from addressbook import Record, AddressBook
-from ui import get_phone, get_email, get_name, get_birthday, get_address, get_old_phone, get_new_phone, get_new_email, \
-    get_old_email, draw_record, draw_contacts
-from ui import draw_notes, draw_single_note
-from notes import Note, NotesManager
-from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
 
-notes_manager = NotesManager()
+from addressbook import AddressBook, Record
+from notes import Note, NotesManager
+from ui import (draw_contacts, draw_record, draw_single_note, get_address,
+                get_birthday, get_email, get_name, get_new_email,
+                get_new_phone, get_old_email, get_old_phone, get_phone)
 
 
 def add_contact(book: AddressBook):
@@ -136,30 +135,40 @@ def show_birthdays(book: AddressBook):
         print("Birthdays not found")
 
 
-def add_note_function():
+def add_note_function(notes_manager: NotesManager):
     title = input("Enter note title: ")
     content = input("Enter note content: ")
-    tags = [tag.strip() for tag in input("Enter tags (comma separated): ").split(",") if tag.strip()]
+    tags = [
+        tag.strip()
+        for tag in input("Enter tags (comma separated): ").split(",")
+        if tag.strip()
+    ]
     note = Note(title, content, tags)
     notes_manager.notes.append(note)
     print("\n✅ Note added.")
     draw_single_note(note)
 
 
-def edit_note_function():
+def edit_note_function(notes_manager: NotesManager):
     title = input("Enter the title of the note you want to edit: ")
     note = notes_manager.find_note_by_title(title)
     if note:
         new_title = input(f"Enter new title (current: {note.title}): ")
         new_content = input(f"Enter new content (current: {note.content}): ")
-        new_tags = [tag.strip() for tag in input(f"Enter new tags (current: {', '.join(note.tags)}): ").split(",") if tag.strip()]
+        new_tags = [
+            tag.strip()
+            for tag in input(
+                f"Enter new tags (current: {', '.join(note.tags)}): "
+            ).split(",")
+            if tag.strip()
+        ]
         notes_manager.edit_note(note, new_title, new_content, new_tags)
         draw_single_note(note)
     else:
         print("[!] Note not found.")
 
 
-def remove_note_function():
+def remove_note_function(notes_manager: NotesManager):
     title = input("Enter the title of the note you want to remove: ")
     note = notes_manager.find_note_by_title(title)
     if note:
@@ -169,10 +178,10 @@ def remove_note_function():
         print("[!] Note not found.")
 
 
-
-
-def search_notes_function():
-    tag_completer = WordCompleter(notes_manager.get_autocomplete_words(), ignore_case=True)
+def search_notes_function(notes_manager: NotesManager):
+    tag_completer = WordCompleter(
+        notes_manager.get_autocomplete_words(), ignore_case=True
+    )
     session = PromptSession(completer=tag_completer)
 
     search_term = session.prompt("🔍 Enter the tag to search for: ").strip()
@@ -184,7 +193,7 @@ def search_notes_function():
         print("[!] No matching notes found by tag.")
 
 
-def show_all_notes_function():
+def show_all_notes_function(notes_manager: NotesManager):
     notes = notes_manager.get_all_notes()
     if notes:
         notes_manager.display_notes(notes)
