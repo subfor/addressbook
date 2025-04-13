db96620066b57e759a02e0994112de8fca53b164
Oleksandr Matvieiev <me@oleksmat.com>
2025-04-13 15:30:05 +0300

    refactor: create app_context, app_state

---
+++ app_state.py
@@ -0,0 +1,18 @@
+from addressbook import AddressBook
+from notes import NotesManager
+
+class AppState:
+    def __init__(self, *, notes: NotesManager, book: AddressBook):
+        self.book = book
+        self.notes = notes
+
+    @staticmethod
+    def load(*, notes_file: str = None, book_file: str = None) -> "AppState":
+        notes = NotesManager.load(notes_file) if notes_file else NotesManager.load()
+        book = AddressBook.load(book_file) if book_file else AddressBook.load()
+
+        return AppState(notes=notes, book=book)
+
+    def save(self):
+        self.book.save()
+        self.notes.save()
